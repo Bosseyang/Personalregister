77cbea8f6b3ebeb56992fdc34ed678598e0ec425
Bosseyang <bosse_yang@hotmail.com>
2025-05-21 16:26:58 +0200

    Programmet klart enligt basic kravspecifikationerna. Har även refaktoriserat koden och lagt metoderna i filen Utilities.cs

--- Personalregister/Program.cs
+++ Personalregister/Program.cs
@@ -1,12 +1,58 @@
-﻿namespace Personalregister
+﻿//Uppgift 1: Vilka klasser bör ingå i programmet? 
+//De bör ha en klass för anställda (employee), samt en för listan av anställda
+//EmployeeRegister
+//Uppgift 2: Vilka attribut och metoder bör ingå i dessa klasser?
+//Klassen Employee bör ha:
+//Attributen: Name, Salary
+//Klassen EmployeeRegister bör ha:
+//Attributen: List<Employee> Employees
+//Metod: Vi använder oss av .Add() samt .ToString()
+//Vi skapar även metoderna AddEmployer och PrintRegistry som lägger
+//till en anställd i listan med klassen Employee, och PrintRegistry skriver
+//ut listan på anställdas namn och lön.
+
+
+namespace Personalregister
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Utilities.RegisterEmployee register = new();
+            bool running = true;
 
-            //C# Övning 1 - Personalregister
-            Console.WriteLine("Hello, World!");
-        }
-    }
+            do
+            {
+                Console.WriteLine("\n*** Menu ***");
+                Console.WriteLine("1. Add new employee");
+                Console.WriteLine("2. Show register");
+                Console.WriteLine("3. Exit program");
+                Console.WriteLine("Please make a choice: ");
+
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        Console.Write("\nEnter employee name: ");
+                        string name = Console.ReadLine();
+                        Console.Write("Enter employee salary in kr (SEK): ");
+                        decimal salary = decimal.Parse(Console.ReadLine());
+                        register.AddEmployee(name, salary);
+                        break;
+                    case "2":
+                        register.PrintRegistry();
+                        break;
+                    case "3":
+                        running = false;
+                        Console.WriteLine("Program exited successfully!");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice, please try again.");
+
+                        break;
+                }
+
+            } while (running);
+        } 
+    } 
 }
---
+++ Personalregister/Utilities.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personalregister
+{
+    internal class Utilities
+    {
+        public class RegisterEmployee()
+        {
+            private List<Employee> Employees = new List<Employee>();
+
+            public void AddEmployee(string name, decimal salary)
+            {
+                Employees.Add(new Employee(name, salary));
+            }
+
+            public void PrintRegistry()
+            {
+                Console.WriteLine("\n*** Employee Registry ***");
+                int i = 1;
+                foreach (var employee in Employees)
+                {
+                    Console.WriteLine("Employee number " + i + ": " + employee);
+                    i++;
+                }
+
+            }
+
+        }
+
+        public class Employee(string name, decimal salary)
+        {
+            public string Name { get; set; } = name;
+            public decimal Salary { get; set; } = salary;
+
+            public override string ToString()
+            {
+                return $"Name: {Name}, Salary: {Salary} kr";
+            }
+        }
+    }
+}
+
