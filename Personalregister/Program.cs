9ac85ff3dad2c8b8a0824c0949219d3fed735320
Bosseyang <bosse_yang@hotmail.com>
2025-05-21 14:36:45 +0200

    Add project files.

---
+++ Personalregister/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Personalregister
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //C# Övning 1 - Personalregister
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
